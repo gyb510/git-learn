bed4652f78449a36f5aa3a7b3e148524d1ee0a55
allenguo <allenguo@slightech.com>
2019-07-03 13:17:26 +0800

    featue3

--- cycx3_uvcdscr.c
+++ cycx3_uvcdscr.c
@@ -43,14 +43,6 @@ const uint8_t CyCx3USB30DeviceDscr[] =
     0xC3, 0x00,                         /* Product ID */
     0x03, 0x01,                         /* Device release number */
 
-    0x01,                               /* Device protocol */
-    0x40,                               /* Maxpacket size for EP0 : 64 bytes */
-
-    0x03,                               /* Serial number string index */
-    0x01                                /* Number of configurations */
-};
-
-/* Binary Device Object Store (BOS) Descriptor */
 const uint8_t CyCx3USBBOSDscr[] =
 {
     0x05,                               /* Descriptor size */
